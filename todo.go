2b920feea8f21dc66ab6644ee51ff3e49e1b9536
Jack Anderson <jack.anderson@bjss.com>
2025-03-22 10:50:53 +0000

    read with scanner

--- todo.go
+++ todo.go
@@ -3,6 +3,7 @@ package main
 import (
 	// "crypto/sha256"
 	"encoding/json"
+	"bufio"
 	"fmt"
 	"hash"
 	"os"
@@ -30,6 +31,7 @@ type ToDoList interface {
 type ToDoListJson struct {
 	jsonFileName string
 	jsonFile     *os.File
+	jsonFileScanner *bufio.Scanner
 }
 
 func NewToDoListJson() (td *ToDoListJson) {
@@ -43,29 +45,35 @@ func NewToDoListJson() (td *ToDoListJson) {
 // In the current directory, initialise a new json to do list
 // if one already exists, do nothing
 func (td *ToDoListJson) Init() {
-	file, err := os.OpenFile(td.jsonFileName, os.O_CREATE | os.O_APPEND | os.O_WRONLY, 0666)
+	writeFile, err := os.OpenFile(td.jsonFileName, os.O_CREATE | os.O_APPEND | os.O_WRONLY, 0666)
 	if err != nil {
 		fmt.Println("Error opening file", err.Error())
 		return
 	}
 	fmt.Println("Initialised")
-	td.jsonFile = file
+	td.jsonFile = writeFile
+	readFile, _:= os.OpenFile(td.jsonFileName, os.O_RDONLY, 0666)
+	td.jsonFileScanner = bufio.NewScanner(readFile)
 }
 
 func (td *ToDoListJson) Add(item ToDoListItem) {
 	bytes, _ := json.Marshal(item)
-	n, err := td.jsonFile.Write(bytes)
+	_, err := td.jsonFile.WriteString(fmt.Sprintf("%s\n", string(bytes)))
 	if(err != nil){
 		fmt.Println("Error writing file", err.Error())
 	}
-	fmt.Println(n)
-	b := make([]byte, 10)
-	td.jsonFile.Read(b)
-	fmt.Println(string(b))
 }
 
 func (td *ToDoListJson) Remove(item ToDoListItem) {
-
+	// This function is best effort. First tries to remove by a matching hash, then by a matching name
+	// failing that, Pop is called to remove the most recent item
+	scanner := td.jsonFileScanner
+	tdItem := &ToDoListItem{}
+	for scanner.Scan(){
+		fmt.Println(scanner.Text())
+		json.Unmarshal(scanner.Bytes(), &tdItem)
+	}
+	fmt.Println(tdItem.Do)
 }
 
 func (td *ToDoListJson) Pop() {
@@ -76,5 +84,7 @@ func main() {
 	td := NewToDoListJson()
 	td.Add(ToDoListItem{
 		Do: "abc",
+	})
+	td.Remove(ToDoListItem{
 	})
 }
